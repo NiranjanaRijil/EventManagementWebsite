3cbde8434190410dc266d2ec2d932ab1d29ffd5c
NiranjanaRijil <niranjanarijil3003@gmail.com>
2026-03-26 22:02:46 +0530

    Update save_feedback.php

--- save_feedback.php
+++ save_feedback.php
@@ -1,11 +1,26 @@
 <?php
 include "db.php";
 
-$name = $_POST['name'];
-$message = $_POST['message'];
+// Check if form was submitted
+if(isset($_POST['name'], $_POST['message'])) {
 
-mysqli_query($conn,"INSERT INTO feedback(name,message) VALUES('$name','$message')");
+    // Sanitize user input
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $message = mysqli_real_escape_string($conn, $_POST['message']);
 
-header("Location: thankyou.php");
-exit();
+    // Insert feedback into database
+    $result = mysqli_query($conn, "INSERT INTO feedback(name,message) VALUES('$name','$message')");
+
+    // Check if insert was successful
+    if(!$result){
+        die("Feedback submission failed: " . mysqli_error($conn));
+    }
+
+    // Redirect to thankyou page
+    header("Location: thankyou.php");
+    exit();
+
+} else {
+    die("Form not submitted correctly!");
+}
 ?>
